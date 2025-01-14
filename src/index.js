4d739162abf6cd2325028eef9d8d9a3395726e0f
enriqueBuelna <jesusenriqueyt@gmail.com>
2025-01-14 16:32:58 -0700

    Build

--- src/index.js
+++ src/index.js
@@ -34,11 +34,12 @@ import { BlockedUsers } from "./models/blocked_users.model.js";
 
 const app = express();
 const PORT = process.env.PORT;
+const URL = process.env.URL;
 
 const server = createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:4200", "http://127.0.0.1:4200"], // URL de tu frontend
+    origin: [URL, URL], // URL de tu frontend
     // origin: ["http://localhost:5500", "http://127.0.0.1:5500"],
     methods: ["GET", "POST"],
     allowedHeaders: ["my-custom-header"],
